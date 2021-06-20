1ef33e75fc9f87244920739ae6a711c6b35aba40
Eijaz Baganee <Eijaz.Baganee@mu.ey.com>
2021-06-20 11:51:48 +0400

    Dropdown widget

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,28 +8,24 @@ export default function Search() {
 
     console.log(results);
 
-    useEffect(() => { // function inside useEffect will be run everytime 'term' is changed
-        
+    useEffect(() => {
         const search = async () => {
-            const { data } =  await axios.get('https://en.wikipedia.org/w/api.php', {
-                params: {
-                    action: "query",
-                    list: "search",
-                    origin: '*',
-                    srsearch: term,
-                    format: "json"
-                }
-            });
-
-            setResults(data.query.search);
-        };       
-        
-        // Will do search if term is not an empty string
-        if(term){
-            search();
+          const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+            params: {
+              action: 'query',
+              list: 'search',
+              origin: '*',
+              format: 'json',
+              srsearch: term,
+            },
+          });
+     
+          setResults(data.query.search);
+        };
+        if (term) {
+          search();
         }
-        
-    }, [term]);
+      }, [term]);
 
     const renderedResults = results.map((result) => {
         return (
